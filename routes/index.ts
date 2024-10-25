b2b8168512e738bbb38ce7be413e5ae458b549b3
Fabio da Silva de Oliveira <dasilvadeoliveirafabio@gmail.com>
2024-10-25 20:34:07 -0300

    teste deploy 16

--- routes/index.ts
+++ routes/index.ts
@@ -2,7 +2,7 @@ import { routeType } from "../types";
 import usuarioRouter from "./usuarioRouter";
 
 export const router:routeType = [
-    {endpoint:"/listar",route:usuarioRouter},
+    {endpoint:"/",route:usuarioRouter},
     {endpoint:"/endereco",route:usuarioRouter},
 
 ]
