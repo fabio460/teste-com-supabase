a59199d83e74322257b3b7025e245415f1df857b
Fabio da Silva de Oliveira <dasilvadeoliveirafabio@gmail.com>
2024-10-25 19:32:09 -0300

    teste deploy 7

--- index.ts
+++ index.ts
@@ -2,11 +2,11 @@ import  express, { Request, Response } from "express";
 import { router } from "./routes";
  const app = express()
  
-//  router.map(e=>{
-//     app.use(e.endpoint, e.route)
-//  })
-app.get('/',(req:Request, res:Response)=>{
-   res.send("olaaaaa")
-})
+ router.map(e=>{
+    app.use(e.endpoint, e.route)
+ })
+// app.get('/',(req:Request, res:Response)=>{
+//    res.send("olaaaaa")
+// })
 
  app.listen(4000,()=>console.log("aplicação rodando na porta 4000!"))
